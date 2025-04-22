ba0c967d8a29511fa3e42508648e87e79ba43768
cloudnatived <zuniq@mail.com>
2025-04-22 13:40:00 +0800

    some_message

---
+++ NCCL/nccl-20250417.py
@@ -0,0 +1,17 @@
+import ctypes
+so_file = "/lib/x86_64-linux-gnu/libnccl.so.2"
+nccl = ctypes.CDLL(so_file)
+ncclResult_t = ctypes.c_int
+class NcclUniqueId(ctypes.Structure):
+    _fields_ = [("internal", ctypes.c_byte * 128)]
+_c_ncclGetUniqueId = nccl.ncclGetUniqueId
+_c_ncclGetUniqueId.restype = ctypes.c_int
+_c_ncclGetUniqueId.argtypes = [ctypes.POINTER(NcclUniqueId)]
+def ncclGetUniqueId() -> NcclUniqueId:
+    unique_id = NcclUniqueId()
+    result = _c_ncclGetUniqueId(ctypes.byref(unique_id))
+    assert result == 0
+    return unique_id
+
+unique_id = ncclGetUniqueId()
+print(list(unique_id.internal))
