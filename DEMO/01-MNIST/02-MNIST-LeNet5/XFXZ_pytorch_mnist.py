7692370d095ba0384a0572f5e25210eeae2e5fe0
cloudnatived <zuniq@mail.com>
2025-11-24 14:08:00 +0800

    Update XFXZ_pytorch_mnist.py

--- DEMO/01-MNIST/02-MNIST-LeNet5/XFXZ_pytorch_mnist.py
+++ DEMO/01-MNIST/02-MNIST-LeNet5/XFXZ_pytorch_mnist.py
@@ -24,7 +24,7 @@ torch.manual_seed(random_seed)
  
 train_loader = torch.utils.data.DataLoader(
     #torchvision.datasets.MNIST('./data/', train=True, download=True,
-    torchvision.datasets.MNIST('/Data/DEMO/MODEL/MNIST', train=True, download=True,
+    torchvision.datasets.MNIST('/Data/DEMO/MODEL/MNIST/', train=True, download=True,
                                transform=torchvision.transforms.Compose([
                                    torchvision.transforms.ToTensor(),
                                    torchvision.transforms.Normalize(
@@ -33,7 +33,7 @@ train_loader = torch.utils.data.DataLoader(
     batch_size=batch_size_train, shuffle=True)
 test_loader = torch.utils.data.DataLoader(
     #torchvision.datasets.MNIST('./data/', train=False, download=True,
-    torchvision.datasets.MNIST('/Data/DEMO/MODEL/MNIST', train=False, download=True,                           
+    torchvision.datasets.MNIST('/Data/DEMO/MODEL/MNIST/', train=False, download=True,                           
                                transform=torchvision.transforms.Compose([
                                    torchvision.transforms.ToTensor(),
                                    torchvision.transforms.Normalize(
